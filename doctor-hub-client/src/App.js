400bc6716c5883adddf2db53d3380603b8cc9643
Himanshu Jain <hjmanojain.8@gmail.com>
2020-04-06 00:05:49 +0530

    Fix React.js syntax

--- doctor-hub-client/src/App.js
+++ doctor-hub-client/src/App.js
@@ -4,46 +4,21 @@ import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Review from "./components/Review";
 import DocForm from "./components/DocForm";
-import loadScript from "./loadScript";
 
 class App extends React.Component {
-  state = {
-    hasRefreshed: false
-  };
-  handlePageChange = () => {
-    if (!this.state.hasRefreshed) {
-      window.location.reload();
-      this.setState({ hasRefreshed: true });
-      setTimeout(() => this.setState({ hasRefreshed: false }), 5000);
-    }
-    console.log("HERE");
-  };
-
   render() {
     return (
       <div>
-        <base href="/"></base>
         <Router>
           <Navbar />
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => {
-                // this.handlePageChange();
-                return <Home />;
-              }}
-            ></Route>
-            <Route
-              exact
-              path="/register/doctor"
-              render={() => {
-                // this.handlePageChange();
-                return <DocForm />;
-              }}
-            ></Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/register/doctor">
+              <DocForm />>
+            </Route>
           </Switch>
           <Footer />
         </Router>
